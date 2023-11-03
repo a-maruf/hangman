c82dfa7fee2cae3cbd8161386eff599268cbe370
afzalm <afzal_maruf@yahoo.co.uk>
2023-11-03 15:57:12 +0000

    Mileston 2, Task 3: Input a single charecter into variable 'guess'

--- milestone_2.py
+++ milestone_2.py
@@ -1,10 +1,14 @@
-import random
+import random   # Part of Task 2
 
+# Task 1
 favorite_fruits = ['Apple', 'Banana', 'Cherry', 'Date', 'Elderberry']
-
 word_list = favorite_fruits
-
 print(word_list)
 
+# Task 2
 word = random.choice(word_list)
 print(word)
+
+# Task 3
+guess = input("\nEnter a single letter: ")
+print(guess)
