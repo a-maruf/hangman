c78f93943021afcad12b80a35060e06eda6756cb
afzalm <afzal_maruf@yahoo.co.uk>
2023-11-03 11:30:52 +0000

    Complete Task 1 Steps 1, 2, and 3 of Milestone 2: (1) List of 5 fruits, (2) Assign to word_list, (3) Print

--- milestone_2.py
+++ milestone_2.py
@@ -1,1 +1,8 @@
+# Step 1: Create a list containing the names of your 5 favorite fruits.
+favorite_fruits = ['Apple', 'Banana', 'Cherry', 'Date', 'Elderberry']
 
+# Step 2: Assign this list to a variable called word_list.
+word_list = favorite_fruits
+
+# Step 3: Print out the newly created list to the standard output (screen).
+print(word_list)
