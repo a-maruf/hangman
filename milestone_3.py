b33b2fd289f551b98225b492174217b024a9f986
afzalm <afzal_maruf@yahoo.co.uk>
2023-11-03 21:38:39 +0000

    Milestone 3, Task 2: Check whether the guess is in the word - using 'if-else' statements

--- milestone_3.py
+++ milestone_3.py
@@ -13,3 +13,16 @@ while True:
     else:
         # Step 5: If the guess does not pass the checks, print a message
         print("Invalid letter. Please, enter a single alphabetical character.")
+
+
+# Task 2: Check whether the guess is in the word
+
+word = "apple"     # Used for test purposes
+
+# Step 1: Create an if statement that checks if the guess is in the word
+if guess in word:
+    # Step 2: In the body of the if statement, print a message saying "Good guess! {guess} is in the word."
+    print(f"Good guess! {guess} is in the word.")
+else:
+    # Step 3: Create an else block that prints a message saying "Sorry, {guess} is not in the word. Try again."
+    print(f"Sorry, {guess} is not in the word. Try again.")
