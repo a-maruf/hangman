e0d4cdd4696d320c61d2554f7fd888237120ac97
afzalm <afzal_maruf@yahoo.co.uk>
2023-11-04 14:51:35 +0000

    Milestone 4, Task 1: Create a new script called milestone_4.py. Define the __init__ method of the Hangman class.01

---
+++ milestone_4.py
@@ -0,0 +1,20 @@
+# Milestone 4, Task 1
+import random
+
+class Hangman:
+    def __init__(self, word_list, num_lives=5):
+        self.word = random.choice(word_list)
+        self.word_guessed = ['_' for _ in self.word]
+        self.num_letters = len(set(self.word))
+        self.num_lives = num_lives
+        self.word_list = word_list
+        self.list_of_guesses = []
+
+# Test the class with a list of words
+hangman = Hangman(['apple', 'banana', 'cherry'])
+print(hangman.word)
+print(hangman.word_guessed)
+print(hangman.num_letters)
+print(hangman.num_lives)
+print(hangman.word_list)
+print(hangman.list_of_guesses)
